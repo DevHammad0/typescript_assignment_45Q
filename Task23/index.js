6aafdb8f0d728ed2ef374fb41bca678e957f6a5e
DevHammad0 <devhammad.m@gmail.com>
2023-08-12 17:57:59 +0500

    Task23 added

---
+++ Task23/index.js
@@ -0,0 +1,44 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing
+ each test and your prediction for the results of each test. Your code should look
+ something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+var age = 18;
+console.log("Is age >= 21? I predict False.");
+console.log(age >= 21);
+var n = "Alice";
+console.log("Is name != 'Bob'? I predict True.");
+console.log(n != "Bob");
+var score = 90;
+console.log("Is score < 100? I predict True.");
+console.log(score < 100);
+var color = "red";
+console.log("Is color == 'blue'? I predict False.");
+console.log(color == "blue");
+var number = 10;
+console.log("Is number > 5? I predict True.");
+console.log(number > 5);
+var animal = "cat";
+console.log("Is animal == 'dog'? I predict False.");
+console.log(animal == "dog");
+var height = 180;
+console.log("Is height <= 200? I predict True.");
+console.log(height <= 200);
+var fruit = "apple";
+console.log("Is fruit != 'banana'? I predict True.");
+console.log(fruit != 'banana');
+var grade = "A";
+console.log("Is grade == 'B'? I predict False.");
+console.log(grade == "B");
+var year = 2023;
+console.log("Is year < 2020? I predict False.");
+console.log(year < 2020);
---
+++ Task23/index.ts
@@ -0,0 +1,60 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing
+ each test and your prediction for the results of each test. Your code should look 
+ something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. 
+*/
+
+
+
+let age = 18;
+console.log("Is age >= 21? I predict False.");
+console.log(age >= 21);
+
+let n = "Alice";
+console.log("Is name != 'Bob'? I predict True.");
+console.log(n != "Bob");
+
+let score = 90;
+console.log("Is score < 100? I predict True.");
+console.log(score < 100);
+
+let color = "red";
+console.log("Is color == 'blue'? I predict False.");
+console.log(color == "blue");
+
+let number = 10;
+console.log("Is number > 5? I predict True.");
+console.log(number > 5);
+
+let animal = "cat";
+console.log("Is animal == 'dog'? I predict False.");
+console.log(animal == "dog");
+
+let height = 180;
+console.log("Is height <= 200? I predict True.");
+console.log(height <= 200);
+
+let fruit = "apple";
+console.log("Is fruit != 'banana'? I predict True.");
+console.log(fruit != 'banana');
+
+let grade = "A";
+console.log("Is grade == 'B'? I predict False.");
+console.log(grade == "B");
+
+let year = 2023;
+console.log("Is year < 2020? I predict False.");
+console.log(year < 2020);
+
+
+
+
