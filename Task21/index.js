471d7b677208dff5263b8577f0e416f83aec0af0
DevHammad0 <devhammad.m@gmail.com>
2023-08-12 17:31:09 +0500

    Task21 added

---
+++ Task21/index.js
@@ -0,0 +1,8 @@
+/*They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items.*/
+var obj = {
+    name: "Muhammad Hammad",
+    age: 19,
+    hobby: ["Playing Table Tennis", "Learning", "Coding"]
+};
+console.log(obj);
---
+++ Task21/index.ts
@@ -0,0 +1,10 @@
+/*They think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items.*/
+
+let obj = {
+    name: "Muhammad Hammad",
+    age: 19,
+    hobby: ["Playing Table Tennis","Learning","Coding"]
+}
+
+console.log(obj);
