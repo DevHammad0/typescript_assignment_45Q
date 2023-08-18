41f5d04e5d5df0effed5f68cc38ed42a2031d7ce
DevHammad0 <devhammad.m@gmail.com>
2023-08-19 02:11:11 +0500

    Task41 added

---
+++ Task41/index.ts
@@ -0,0 +1,9 @@
+let magicians: string[] = ["David Copperfield", "Dynamo", "Penn & Teller", "David Blaine"];
+
+function show_magicians(magicians: string[]): void {
+  for (let magician of magicians) {
+    console.log(magician);
+  }
+}
+
+show_magicians(magicians);
