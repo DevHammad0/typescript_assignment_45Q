45cb45ec0650362299338d855fb6ff6acf1cb9c2
DevHammad0 <devhammad.m@gmail.com>
2023-08-19 01:48:09 +0500

    Task36 added

---
+++ Task36/index.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("You have ordered a ".concat(size, " shirt with the message \"").concat(message, "\" on it."));
+}
+make_shirt("large", "Hello World");
---
+++ Task36/index.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string,message: string) {
+    console.log(`You have ordered a ${size} shirt with the message "${message}" on it.`);
+  }
+  
+  make_shirt("large", "Hello World");
+  
