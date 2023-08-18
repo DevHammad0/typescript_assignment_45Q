40cc7546d831b4523e6ea1680c542d2c2c77214c
DevHammad0 <devhammad.m@gmail.com>
2023-08-19 02:39:24 +0500

    Task44 added

---
+++ Task44/index.js
@@ -0,0 +1,19 @@
+function make_sandwich(items) {
+    var summary = "You ordered a sandwich with ";
+    for (var i = 0; i < items.length; i++) {
+        summary += items[i];
+        // Adding a comma or and if there are more items
+        if (i < items.length - 2) {
+            summary += ", ";
+        }
+        else if (i == items.length - 2) {
+            summary += ", and ";
+        }
+    }
+    //adding fullstop at end
+    summary += ".";
+    console.log(summary);
+}
+make_sandwich(["cheese", "ham", "lettuce", "tomato"]);
+make_sandwich(["peanut butter", "jelly"]);
+make_sandwich(["chicken", "catchup", "mayo", "mustard"]);
