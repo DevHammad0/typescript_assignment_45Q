dc2816408b0a28b06404f63ac45f8660a3f16fe2
DevHammad0 <devhammad.m@gmail.com>
2023-09-23 13:32:23 +0500

    added types explicitly

--- Task7/index.ts
+++ Task7/index.ts
@@ -1,6 +1,6 @@
 //7
-let add = 2+6;
-let sub= 10-2;
-let div= 16/2;
-let mul = 4*2;
+let add:number = 2+6;
+let sub:number= 10-2;
+let div:number= 16/2;
+let mul:number = 4*2;
 console.log(`2 + 6 = ${add}\n10 - 2 = ${sub}\n16 / 2 = ${div}\n4 * 2 = ${mul}`);
